9290982637f184e71b1e429ea5db72dbb4634c1e
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2025-01-09 05:54:36 +0300

    controller update

--- GeyikLoung/Controllers/Admin/ProductController.cs
+++ GeyikLoung/Controllers/Admin/ProductController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using GeyikLoung.Context;
+using GeyikLoung.Entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,99 @@ namespace GeyikLoung.Controllers.Admin
 {
     public class ProductController : Controller
     {
-        // GET: Product
+        private readonly GeyikLoungContext _context;
+
+        public ProductController()
+        {
+            _context = new GeyikLoungContext();
+        }
+
         public ActionResult Index()
         {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(_context.Categories, "Id", "Name");
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product, HttpPostedFileBase imageFile)
+        {
+            if (imageFile != null)
+            {
+                string imagePath = Path.Combine(Server.MapPath("~/images"), Path.GetFileName(imageFile.FileName));
+                imageFile.SaveAs(imagePath);
+                product.ImagePath = "/images/" + Path.GetFileName(imageFile.FileName);
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, HttpPostedFileBase imageFile)
+        {
+            var existingProduct = _context.Products.Find(product.Id);
+            if (existingProduct != null)
+            {
+                existingProduct.Name = product.Name;
+                existingProduct.Price = product.Price;
+                existingProduct.CategoryId = product.CategoryId;
+                if (imageFile != null)
+                {
+                    string imagePath = Path.Combine(Server.MapPath("~/images"), Path.GetFileName(imageFile.FileName));
+                    imageFile.SaveAs(imagePath);
+                    existingProduct.ImagePath = "/images/" + Path.GetFileName(imageFile.FileName);
+                }
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
